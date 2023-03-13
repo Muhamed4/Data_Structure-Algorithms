fcd28b400394f9534b575c3dd69b031deae9df67
Muhamed4 <mohamedmorsi722@gmail.com>
2023-03-13 12:41:53 +0200

    Practice on Pointer using c++

---
+++ Pointers/Pointer_Practice/Pointer1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+void practice1(){
+    // Each Segment in memory has 1 byte of memory.
+    // Each byte of the memeory has an address.
+    // When we declare a variable and execute the program, the computer allocates some amount of memory for this variable.
+    // How much memory allocates for this variable ? It depends on the data type and the compiler.
+    // The Computer has an internal structure, a lookup table where it stores this information that there is a variable (name of varaible) , it's of type interger , and it is located at address (xxxxxxxxx) which is the starting address of the variable
+}
+
+int main(){
+
+
+    practice1();
+    
+
+
+
+    return 0;
+}
